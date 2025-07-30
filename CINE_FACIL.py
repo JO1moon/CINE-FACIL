ca95f0d39a00daa88b1db06ae75f7c8994be1f4a
jose <jo497022@gmail.com>
2025-07-29 20:43:34 -0600

    cambios en el archivo

--- CINE_FACIL.py
+++ CINE_FACIL.py
@@ -1,8 +1,34 @@
 peliculas = ["Los juegos del hambre", "El club de la pelea", "El gran truco", 
              "Gladiador", "Tiempos violentos"]
 
+precios = {
+    "Los juegos del hambre": 25,
+    "El club de la pelea": 50,
+    "El gran truco": 40,
+    "Gladiador": 80,
+    "Tiempos violentos": 100
+}
+
 reservas = []
 
+def cambiar_precio():
+    print("\n--- Cambiar precio de una película ---")
+    for peli in peliculas:
+        print(f"{peli}: Q{precios[peli]}")
+    nombre = input("Ingrese el nombre de la película cuyo precio desea cambiar: ")
+    if nombre in peliculas:
+        nuevo_precio = int(input(f"Ingrese el nuevo precio para '{nombre}': "))
+        precios[nombre] = nuevo_precio
+        print(f"Precio actualizado. Nuevo precio de '{nombre}': Q{nuevo_precio}")
+    else:
+        print("La película no está en la lista.")
+
+
+pregunta=input("Deseas cambiar el precio de una pelicula (s/n)")
+if pregunta == "s":
+    cambiar_precio()
+elif pregunta == "n":
+    print()
 print("Películas disponibles:")
 for i in peliculas:
     print("-", i)
@@ -15,15 +41,15 @@ costo = 0
 
 if pelicula in peliculas:
     if pelicula == "Los juegos del hambre":
-        costo = boletos * 25
+        costo = boletos * precios["Los juegos del hambre"]
     elif pelicula == "El club de la pelea":
-        costo = boletos * 50
+        costo = boletos * precios["El club de la pelea"]
     elif pelicula == "El gran truco":
-        costo = boletos * 40
+        costo = boletos * precios["El gran truco"]
     elif pelicula == "Gladiador":
-        costo = boletos * 80
+        costo = boletos * precios["Gladiador"]
     elif pelicula == "Tiempos violentos":
-        costo = boletos * 100
+        costo = boletos * precios["Tiempos violentos"]
     
     print("Total a pagar:", costo)
 
